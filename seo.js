81e9e0892d4af8be77231d3ed7a05c09520a112f
bstocks <bstocks@users.noreply.github.com>
2014-10-28 15:50:27 -0700

    Customize for Meteor LA
    Change primary color variable
    Update header and SEO to say ‘Meteor LA’
    Add sponsor image for Ongo Works

--- seo.js
+++ seo.js
@@ -1,9 +1,9 @@
 Meteor.startup(function() {
   if(Meteor.isClient) {
     SEO.config({
-      title: 'Meteor Vegas',
+      title: 'Meteor LA',
       meta: {
-        'description': 'A Bi-Monthly Meteor.js Meetup in Las Vegas, NV'
+        'description': 'Meteor.js Meetups in Las Angeles, CA'
       },
       og: {
         'image': Meteor.absoluteUrl('share-image.png')
